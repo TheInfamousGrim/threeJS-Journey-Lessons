3008e787d75a0f4d7735086350a78dcc1985d907
TheInfamousGrim <finchergeorge1@gmail.com>
2023-01-24 21:10:37 +0000

    Feat 🎯: Complete 08 debug ui
    Added lil gui so that I can tweak my scenes.
    
    Installed lil gui as a dependency
    
    ALWAYS USE ON EVERY PROJECT!!!!

--- lesson-content/01-basics/08-debug-ui/src/script.js
+++ lesson-content/01-basics/08-debug-ui/src/script.js
@@ -1,6 +1,12 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import gsap from 'gsap';
+import GUI from 'lil-gui';
+
+/* -------------------------------------------------------------------------- */
+/*                                   lil gui                                  */
+/* -------------------------------------------------------------------------- */
+const gui = new GUI();
 
 /**
  * Base
@@ -14,10 +20,41 @@ const scene = new THREE.Scene();
 /**
  * Object
  */
+const parameters = {
+    color: 0xff0000,
+    spin: () => {
+        gsap.to(mesh.rotation, {
+            duration: 1,
+            y: mesh.rotation.y + Math.PI * 2,
+        });
+    },
+};
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ color: parameters.color });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+/* ------------------------- li gui UI debugging mesh ----------------------- */
+// Debug
+const meshFolder = gui.addFolder('Mesh Controls');
+meshFolder.add(mesh.position, 'x').min(-3).max(3).step(0.01).name('elevation');
+// gui.add(mesh.position, 'y', -3, 3, 0.01);
+// gui.add(mesh.position, 'z', -3, 3, 0.01);
+
+meshFolder.add(mesh, 'visible');
+
+/* ---------------------------- material lil gui ---------------------------- */
+const materialFolder = gui.addFolder('Material Controls');
+
+materialFolder.add(material, 'wireframe');
+
+// Color for lil gui
+materialFolder.addColor(parameters, 'color').onChange(() => {
+    material.color.set(parameters.color);
+});
+
+// Adding functions
+gui.add(parameters, 'spin');
 
 /**
  * Sizes
