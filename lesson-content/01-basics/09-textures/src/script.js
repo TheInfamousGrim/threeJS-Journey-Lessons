5d21508c6821492a112691907de356a80d0a9fa9
TheInfamousGrim <finchergeorge1@gmail.com>
2023-01-25 03:44:19 +0000

    Feat 🎯: Complete 09 textures

--- lesson-content/01-basics/09-textures/src/script.js
+++ lesson-content/01-basics/09-textures/src/script.js
@@ -1,6 +1,81 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+// Textures
+import doorColorTexture from '../static/textures/door/color.jpg';
+
+/* -------------------------------------------------------------------------- */
+/*                                  Textures                                  */
+/* -------------------------------------------------------------------------- */
+const loadingManger = new THREE.LoadingManager();
+
+loadingManger.onStart = () => {
+    console.log('loadingManager: onStart');
+};
+
+loadingManger.onLoad = () => {
+    console.log('loadingManager: onLoad');
+};
+
+loadingManger.onProgress = () => {
+    console.log('loadingManager: onProgress');
+};
+
+loadingManger.onError = () => {
+    console.log('loadingManager: onError');
+};
+
+const textureLoader = new THREE.TextureLoader(loadingManger);
+// const colorTexture = textureLoader.load('/textures/door/color.jpg');
+// const colorTexture = textureLoader.load('/textures/checkerboard-1024x1024.png');
+// const colorTexture = textureLoader.load('/textures/checkerboard-8x8.png');
+const colorTexture = textureLoader.load('/textures/minecraft.png');
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg');
+const ambientOcclusionTexture = textureLoader.load(
+    '/textures/door/ambientOcclusion.jpg'
+);
+const heightTexture = textureLoader.load('/textures/door/height.jpg');
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg');
+// Normal should always be .png!!!
+const normalTexture = textureLoader.load('/textures/door/normal.jpg');
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg');
+
+/* -------------------------- Transforming textures ------------------------- */
+
+// Repeating textures
+// colorTexture.repeat.x = 2;
+// colorTexture.repeat.y = 3;
+// colorTexture.wrapS = THREE.RepeatWrapping;
+// colorTexture.wrapT = THREE.RepeatWrapping;
+// Mirror Repeat Wrapping
+// colorTexture.wrapS = THREE.MirrorRepeatWrapping;
+// colorTexture.wrapT = THREE.MirrorRepeatWrapping;
+
+// Offset the door texture
+// colorTexture.offset.x = 0.5;
+// colorTexture.offset.y = 0.5;
+
+// Rotate the door texture
+// colorTexture.rotation = Math.PI / 4;
+// colorTexture.center.x = 0.5;
+// colorTexture.center.y = 0.5;
+
+/* ------------------------ Filtering and Midmapping ------------------------ */
+
+// Texture filters
+// WE DON'T NEED MIDMAPS WHEN WE'RE USING TEXTURE FILTERS
+colorTexture.generateMipmaps = false;
+// colorTexture.minFilter = THREE.NearestFilter;
+colorTexture.magFilter = THREE.NearestFilter;
+
+/* --------------------- Texture format and optimisation -------------------- */
+// Three things to keep in mind when using textures
+// 1. The weight (number of texture files)
+// Use jpg where possible
+// You can use sites like tinyPNG for .png files
+// 2. The size (resolution of texture files)
+// 3. The data that is in the textures
+
 /**
  * Base
  */
@@ -14,7 +89,12 @@ const scene = new THREE.Scene();
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+// Texture stuff
+// UV Coordinates
+// console.log(geometry.attributes.uv);
+const material = new THREE.MeshBasicMaterial({ map: colorTexture });
+
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
